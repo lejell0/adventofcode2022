20ab7fc3b86bd877a99bbfea34d11aa293944cc6
lejell0 <followthejell0@gmail.com>
2022-12-06 10:51:25 -0500

    day5 ouch, done

--- day5/day5.js
+++ day5/day5.js
@@ -10,7 +10,7 @@ let stacks = cargo.split('\n').map((line) => line.includes('[')
 
 stacks = stacks.map((stack, i) => {
     let newStack = [];
-    if (i != stack.length-1) {
+    if (i != stack.length) {
     [...Array(stack.length-1).keys()].forEach((it, innerIndex) => {
         let elem = stacks[innerIndex][i];
 
@@ -22,7 +22,7 @@ stacks = stacks.map((stack, i) => {
 return newStack
 })
 
-stacks = stacks.filter(e=>e.length)
+// stacks = stacks.filter(e=>e.length)
 
 const moves = procedure.split("\n").map((moveParse) => {
     const [move, from, to] = Array.from(moveParse.matchAll(/(\d+)/g)).map((m) => m[0]).map(Number);
@@ -30,14 +30,14 @@ const moves = procedure.split("\n").map((moveParse) => {
         move, from: from, to: to
     };
 })
+
 moves.map(({ move, from, to }) => {
     // [ [ 'N', 'Z' ], [ 'D', 'C', 'M' ], [ 'P' ] ]
-    // let resStack = stacks[from -1].splice(0, move).reverse()
-    console.log(to)
+    let resStack = stacks[from -1].splice(0, move)
 
-    // stacks[to-1].unshift('a')
+    stacks[to-1].unshift(...resStack)
     })
 
 
-// const result = stacks.map(arr => arr[0])
-// console.log(result)
+const result = stacks.map(arr => arr[0])
+console.log(result)
